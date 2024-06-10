1a679d850524d5d489405809ea9bf4c87bd65f01
Leonardo Murça <leo@leomurca.xyz>
2024-06-10 16:45:19 -0300

    Add Staggered Grid with images

--- app/src/main/java/xyz/leomurca/flickrfinder/data/repository/fake/FakePhotoRepository.kt
+++ app/src/main/java/xyz/leomurca/flickrfinder/data/repository/fake/FakePhotoRepository.kt
@@ -32,7 +32,7 @@ class FakePhotoRepository @Inject constructor(
                     PhotoMetadata(
                         title = "Porcupine in A Tree",
                         link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
-                        media = "https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg",
+                        media = "https://live.staticflickr.com/65535/53762634202_b9bd80c596_m.jpg",
                         dateTaken = "May 12, 2024",
                         description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
                         published = "June 4, 2024",
@@ -43,7 +43,7 @@ class FakePhotoRepository @Inject constructor(
                     PhotoMetadata(
                         title = "Porcupine in A Tree",
                         link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
-                        media = "https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg",
+                        media = "https://live.staticflickr.com/65535/53758695904_4954b2d59f_m.jpg",
                         dateTaken = "May 12, 2024",
                         description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
                         published = "June 4, 2024",
@@ -54,7 +54,7 @@ class FakePhotoRepository @Inject constructor(
                     PhotoMetadata(
                         title = "Porcupine in A Tree",
                         link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
-                        media = "https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg",
+                        media = "https://live.staticflickr.com/65535/53758182531_6231126679_m.jpg",
                         dateTaken = "May 12, 2024",
                         description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
                         published = "June 4, 2024",
@@ -65,7 +65,7 @@ class FakePhotoRepository @Inject constructor(
                     PhotoMetadata(
                         title = "Porcupine in A Tree",
                         link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
-                        media = "https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg",
+                        media = "https://live.staticflickr.com/65535/53758243558_64630004a4_m.jpg",
                         dateTaken = "May 12, 2024",
                         description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
                         published = "June 4, 2024",
@@ -76,7 +76,74 @@ class FakePhotoRepository @Inject constructor(
                     PhotoMetadata(
                         title = "Porcupine in A Tree",
                         link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
-                        media = "https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg",
+                        media = "https://live.staticflickr.com/65535/53756275486_1bbd31321a_m.jpg",
+                        dateTaken = "May 12, 2024",
+                        description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
+                        published = "June 4, 2024",
+                        author = "nobody@flickr.com (\"ZoomZoom Du Buisson\")",
+                        authorId = "197381316@N02",
+                        tags = listOf("maine", "wildlife", "nature", "outdoor", "spring", "outside", "porcupine", "tree")
+                    ),
+                    PhotoMetadata(
+                        title = "Porcupine in A Tree",
+                        link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
+                        media = "https://live.staticflickr.com/65535/53756195596_9915115f0d_m.jpg",
+                        dateTaken = "May 12, 2024",
+                        description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
+                        published = "June 4, 2024",
+                        author = "nobody@flickr.com (\"ZoomZoom Du Buisson\")",
+                        authorId = "197381316@N02",
+                        tags = listOf("maine", "wildlife", "nature", "outdoor", "spring", "outside", "porcupine", "tree")
+                    ),
+                    PhotoMetadata(
+                        title = "Porcupine in A Tree",
+                        link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
+                        media = "https://live.staticflickr.com/65535/53754473281_6f6deb7ac3_m.jpg",
+                        dateTaken = "May 12, 2024",
+                        description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
+                        published = "June 4, 2024",
+                        author = "nobody@flickr.com (\"ZoomZoom Du Buisson\")",
+                        authorId = "197381316@N02",
+                        tags = listOf("maine", "wildlife", "nature", "outdoor", "spring", "outside", "porcupine", "tree")
+                    ),
+                    PhotoMetadata(
+                        title = "Porcupine in A Tree",
+                        link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
+                        media = "https://live.staticflickr.com/65535/53754887850_6b7ebd227a_m.jpg",
+                        dateTaken = "May 12, 2024",
+                        description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
+                        published = "June 4, 2024",
+                        author = "nobody@flickr.com (\"ZoomZoom Du Buisson\")",
+                        authorId = "197381316@N02",
+                        tags = listOf("maine", "wildlife", "nature", "outdoor", "spring", "outside", "porcupine", "tree")
+                    ),
+
+                    PhotoMetadata(
+                        title = "Porcupine in A Tree",
+                        link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
+                        media = "https://live.staticflickr.com/65535/53751911656_ac08a4a0cd_m.jpg",
+                        dateTaken = "May 12, 2024",
+                        description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
+                        published = "June 4, 2024",
+                        author = "nobody@flickr.com (\"ZoomZoom Du Buisson\")",
+                        authorId = "197381316@N02",
+                        tags = listOf("maine", "wildlife", "nature", "outdoor", "spring", "outside", "porcupine", "tree")
+                    ),
+                    PhotoMetadata(
+                        title = "Porcupine in A Tree",
+                        link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
+                        media = "https://live.staticflickr.com/65535/53752323385_d7dc1a9853_m.jpg",
+                        dateTaken = "May 12, 2024",
+                        description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
+                        published = "June 4, 2024",
+                        author = "nobody@flickr.com (\"ZoomZoom Du Buisson\")",
+                        authorId = "197381316@N02",
+                        tags = listOf("maine", "wildlife", "nature", "outdoor", "spring", "outside", "porcupine", "tree")
+                    ),
+                    PhotoMetadata(
+                        title = "Porcupine in A Tree",
+                        link = "https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/",
+                        media = "https://live.staticflickr.com/65535/53749377942_545fd080df_m.jpg",
                         dateTaken = "May 12, 2024",
                         description = " <p><a href=\"https://www.flickr.com/people/zoomzoomslittleadventures/\">ZoomZoom Du Buisson</a> posted a photo:</p> <p><a href=\"https://www.flickr.com/photos/zoomzoomslittleadventures/53769361124/\" title=\"Porcupine in A Tree\"><img src=\"https://live.staticflickr.com/65535/53769361124_4f4e902d92_m.jpg\" width=\"240\" height=\"160\" alt=\"Porcupine in A Tree\" /></a></p> ",
                         published = "June 4, 2024",
--- app/src/main/java/xyz/leomurca/flickrfinder/ui/home/HomeScreen.kt
+++ app/src/main/java/xyz/leomurca/flickrfinder/ui/home/HomeScreen.kt
@@ -1,13 +1,17 @@
 package xyz.leomurca.flickrfinder.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
@@ -17,18 +21,24 @@ fun HomeScreen(viewModel: HomeViewModel) {
 
     when (val value = state.value) {
         is HomeViewModel.UiState.Loaded -> {
-            LazyColumn {
-                items(value.photos) {
-                    AsyncImage(
-                        model = it.media,
-                        contentDescription = it.description,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(150.dp)
-                    )
-                    Text(text = it.title)
-                }
-            }
+            LazyVerticalStaggeredGrid(
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 4.dp,
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                content = {
+                    items(value.photos) {
+                        AsyncImage(
+                            model = it.media,
+                            contentScale = ContentScale.Crop,
+                            contentDescription = it.description,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight()
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxSize()
+            )
         }
 
         is HomeViewModel.UiState.Loading -> {
